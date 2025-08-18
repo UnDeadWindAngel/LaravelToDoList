57976724918e8e66a1bdbc536a7260a3783d1f00
Vladimir Novikov <UnDeadWindAngel@yandex.ru>
2025-08-18 09:13:24 +0300

    первый проект ларавел

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        return response()->json(Task::create(request()->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update(request()->all());
+        return response()->json($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json([
+            'message' => 'Task deleted successfully'
+        ]);
+    }
+}
